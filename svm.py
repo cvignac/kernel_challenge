81ff9384c853a7b4e087973c12f34994ea74a9da
raphael <raphael.duroselle@polytechnique.edu>
2018-03-23 14:08:16 +0100

    Update svm, add substring kernel

--- svm.py
+++ svm.py
@@ -16,17 +16,30 @@ def signe(x):
 
 class svm :
     
-    def __init__(self, y, G, lambd = 1 ):
+    def __init__(self,C):
         self.support_vectors = []
+        self.y = np.zeros(1)
+        self.alpha = np.zeros(1)
+        self.C = C
+        self.lambd = 1. /C
+        self.K = np.zeros(1)
+    
+    def fit(self,G,y,):
+        self.lambd = 1/C
+        self.K = G
         n, d = np.shape(G)
         self.y = y.astype('double')*2 - np.ones(n)
         self.alpha = np.zeros(n)
-        self.lambd = lambd
-        self.K = G
+        self.calcule_alpha()
+        self.choix_support_vectors()
+   
+    def change_C(self,C):
+        self.C = C
+        self.lambd =1./C
                 
-    def calcule_alpha(self,lambd = -1):
-        if lambd >= 0 :
-            self.lambd = lambd
+    def calcule_alpha(self,C = -1):
+        if C >= 0 :
+            self.change_C(C)
         n,d = np.shape(self.K)
         P = 2 * matrix(self.K,tc='d')
         q = -2 * matrix(self.y,(n,1))
@@ -37,7 +50,7 @@ class svm :
         sol = solvers.qp(P, q, G, h)
         self.alpha = np.array(sol['x'])
         
-    def choix_support_vectors(self,seuil= 0.00000001):
+    def choix_support_vectors(self,seuil= 0):
         self.support_vectors = []
         for i in range(len(self.alpha)):
             if abs(self.alpha[i][0])>= seuil :
